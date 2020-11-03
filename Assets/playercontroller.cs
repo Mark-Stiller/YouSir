28d7445f00caaeaa0d5b731cfd6341a1e3bdc21f
Mark-Stiller <46461833+Mark-Stiller@users.noreply.github.com>
2020-11-03 13:25:26 -0500

    should be fixed on github now

---
+++ Assets/playercontroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    Rigidbody2D body;
+    Vector2 v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        v = Vector2.zero;
+        body.velocity = v;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        v.x = 0;
+        while (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            v.x = h;
+        }
+        body.velocity = v;
+    }
+}
